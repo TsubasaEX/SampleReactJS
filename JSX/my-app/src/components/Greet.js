2fd58d9cff52ad85aa5ab87302ba790d0062685a
zach chih <zach.chih@advantech.com.tw>
2019-12-26 15:06:05 +0800

    up

---
+++ JSX/my-app/src/components/Greet.js
@@ -0,0 +1,11 @@
+//1- This is a functional component
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello World!!</h1>
+// }
+
+//ES6 style
+const Greet = () => <h1>Hello World!!</h1>
+
+export default Greet
