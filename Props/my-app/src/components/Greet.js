4fdb0b6b0a10748d8cfe9a0a945e52d5cacad59b
zach chih <zach.chih@advantech.com.tw>
2019-12-27 11:28:22 +0800

    up

---
+++ Props/my-app/src/components/Greet.js
@@ -0,0 +1,19 @@
+//2- This is a functional component
+import React from 'react';
+
+// function Greet(){
+//     return <h1>Hello World!!</h1>
+// }
+
+//ES6 style
+const Greet = props => {
+    return(
+        <div>
+            <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+            {props.children}
+        </div>
+    )
+}
+
+
+export default Greet
---
+++ Props/my-app/src/components/Welcome.js
@@ -0,0 +1,15 @@
+//2- This is a class component
+import React , {Component} from 'react'
+
+class Welcome extends Component{
+    render(){
+         return (
+            <div>
+                <h1>Welcom {this.props.name} a.k.a {this.props.heroName}</h1>
+                {this.props.children}
+            </div>
+         )
+    }
+}
+
+export default Welcome
