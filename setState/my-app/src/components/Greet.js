0fcc97136937ef7d02cbe1bb9369374984f292b7
TsubasaEX <ss3808924@gmail.com>
2019-12-30 20:41:09 +0800

    setState

---
+++ setState/my-app/src/components/Greet.js
@@ -0,0 +1,20 @@
+//2- This is a functional component
+import React from 'react';
+
+// function Greet(){
+//     return <h1>Hello World!!</h1>
+// }
+
+//ES6 style
+const Greet = props => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+            {props.children}
+        </div>
+    )
+}
+
+
+export default Greet
