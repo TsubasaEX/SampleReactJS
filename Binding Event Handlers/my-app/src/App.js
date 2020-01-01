4c8a6fd47b7dc83f97f4718428ba24f490d7ec5e
TsubasaEX <ss3808924@gmail.com>
2020-01-01 17:40:51 +0800

    up

---
+++ Binding Event Handlers/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+
+function App() {
+  return (
+    <div className="App">
+      <EventBind></EventBind>
+    </div>
+  );
+}
+
+export default App;
