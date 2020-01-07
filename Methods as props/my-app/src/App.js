b70e8e092c1925175c8bff6b1829445700034ebd
zach chih <zach.chih@advantech.com.tw>
2020-01-07 11:14:58 +0800

    up

---
+++ Methods as props/my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
