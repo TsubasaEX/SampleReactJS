ab685de136707d3baf62021fff1993e2cf48bdb8
zach chih <zach.chih@advantech.com.tw>
2020-01-08 15:40:55 +0800

    up

--- Styling and CSS Basics/my-app/src/App.js
+++ Styling and CSS Basics/my-app/src/App.js
@@ -18,14 +18,29 @@ import Inline from './components/Inline';
 import './appStyles.css'
 // method 3 - css module - locally scoped
 import styles from './appStyles.module.css'
+import { Button } from './components/Button';
+import ButtonWrapper from './components/ButtonWrapper';
+import styled from 'styled-components'
+import Link from './components/Link'
+import GlobalStyles from './global-styles'
+
+const CustomLink = styled(Link)`
+  color : red
+`
 
 function App() {
   return (
     <div className="App">
       <h1 className='error'>Error</h1>
       <h1 className={styles.success}>Success</h1>
-      <Inline/>
-      <Stylesheet primary={true}/>
+      <Inline />
+      <Stylesheet primary={true} />
+      <Button>Click Me</Button>
+      <ButtonWrapper clicked={true}>Click Me</ButtonWrapper>
+      <CustomLink to="https://google.com">
+        <h2>Google</h2>
+      </CustomLink>
+      <GlobalStyles/>
     </div>
   );
 }
