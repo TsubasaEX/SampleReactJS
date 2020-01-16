94853b85d97bd620e47b97eb04c7eb0768cfa9d1
zach chih <zach.chih@advantech.com.tw>
2020-01-16 15:38:00 +0800

    up

--- React Hooks/useState Hook/state-hook/src/components/HookCounter.js
+++ React Hooks/useState Hook/state-hook/src/components/HookCounter.js
@@ -4,19 +4,15 @@ function HookCounter() {
 
     //Simple Example
     const [count, setCount] = useState(0)
+
+    // Object Example
+    const [obj, setObj] = useState({ count: 0 })
     return (
         <div>
             <button onClick={() => setCount(count + 1)}>Count {count}</button>
+            <button onClick={() => setObj({ count: obj.count + 1 })}>Count {obj.count}</button>
         </div>
     )
-
-    // Object Example
-    // const [obj, setCount] = useState({ count: 0 })
-    // return (
-    //     <div>
-    //         <button onClick={() => setCount({ count: obj.count + 1 })}>Count {obj.count}</button>
-    //     </div>
-    // )
 }
 
 export default HookCounter
