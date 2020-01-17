a977f4fcc7b891cc2ee7850f3a15e8919d54e93e
zach chih <zach.chih@advantech.com.tw>
2020-01-17 10:10:46 +0800

    up

---
+++ React Hooks/useState with previous state/state-hook/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HookCounterTwo from './components/HookCounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounterTwo/>
+    </div>
+  );
+}
+
+export default App;
