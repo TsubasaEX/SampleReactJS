0947414440921571fe67eb6eb22f9c3ed8efa1fc
zach chih <zach.chih@advantech.com.tw>
2020-01-17 16:11:14 +0800

    up

--- React Hooks/6-Conditionally run effects/effect-hook/src/components/HookCounterOne.js
+++ React Hooks/6-Conditionally run effects/effect-hook/src/components/HookCounterOne.js
@@ -3,14 +3,18 @@ import React, { useState, useEffect } from 'react'
 function HookCounterOne() {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     // will call the function in useEffect after every single render
+    // second parameter is used to identify the conditional variables required checking
     useEffect(() => {
+        console.log('useEffect - updating document title')
         document.title = `Clicked ${count} times`
-    })
+    }, [count])
 
     return (
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
             <button onClick={() => setCount(prevCount => prevCount + 1)}>Click {count} times</button>
         </div>
     )
