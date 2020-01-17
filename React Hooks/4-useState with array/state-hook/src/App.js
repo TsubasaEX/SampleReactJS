644135142a28f64c5735ceb0102922a69c33a1d2
zach chih <zach.chih@advantech.com.tw>
2020-01-17 11:28:08 +0800

    up

---
+++ React Hooks/4-useState with array/state-hook/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HookCounterFour from './components/HookCounterFour';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounterFour />
+    </div>
+  );
+}
+
+export default App;
