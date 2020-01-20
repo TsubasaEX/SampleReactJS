9871901c58ed703ccffab301debc0dfc5c9f0a51
zach chih <zach.chih@advantech.com.tw>
2020-01-20 17:16:29 +0800

    up

---
+++ React Hooks/13-useContext Hook Part 2/context-hook/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ComponentC from './components/ComponentC';
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
+function App() {
+  return (
+    <div className="App">
+
+      <UserContext.Provider value={'Vishwas'}>
+        <ChannelContext.Provider value={'Codevolution'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+export default App;
