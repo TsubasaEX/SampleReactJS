f3926ae9590e9f1ef92d4060f4d7fc79ee483a7e
zach chih <zach.chih@advantech.com.tw>
2020-01-21 14:49:08 +0800

    up

---
+++ React Hooks/16-useReducer (complex state & action)/reducer-hook/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterTwo />
+    </div>
+  );
+}
+
+export default App;
