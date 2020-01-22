ed8ef093f176ff79a81985f5aca63b69b66c4597
zach chih <zach.chih@advantech.com.tw>
2020-01-22 11:00:04 +0800

    up

---
+++ React Hooks/19-Fetching data with useReducer Part 1/reducer-hook/src/App.js
@@ -0,0 +1,17 @@
+import React, { useReducer } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DataFetchingOne from './components/DataFetchingOne';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <DataFetchingOne />
+    </div>
+  );
+}
+
+export default App;
