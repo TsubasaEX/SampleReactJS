f95f02f4ea51c959445badad1fea4ab1a4589a0b
zach chih <zach.chih@advantech.com.tw>
2020-01-30 16:35:26 +0800

    up

---
+++ React Hooks/23-useRef Hook Part 1/ref-hook/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FocusInput from './components/FocusInput';
+
+function App() {
+  return (
+    <div className="App">
+      <FocusInput/>
+    </div>
+  );
+}
+
+export default App;
