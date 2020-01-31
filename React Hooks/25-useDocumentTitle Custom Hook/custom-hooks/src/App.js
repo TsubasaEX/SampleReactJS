ef99add7875cf8d3e33de8fe62ad0a8c992337e8
zach chih <zach.chih@advantech.com.tw>
2020-01-31 15:48:37 +0800

    up

---
+++ React Hooks/25-useDocumentTitle Custom Hook/custom-hooks/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DocTitleOne from './components/DocTitleOne';
+import DocTitleTwo from './components/DocTitleTwo';
+
+function App() {
+  return (
+    <div className="App">
+      <DocTitleOne/>
+      <DocTitleTwo/>
+    </div>
+  );
+}
+
+export default App;
