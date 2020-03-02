85ffff343d157751829e97284b7c6198fe32c325
zach chih <zach.chih@advantech.com.tw>
2020-03-02 15:30:43 +0800

    up

---
+++ React Redux/22-mapDispatchToProps/react-redux-demo/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import CakeContainer from './components/CakeContainer';
+import HookCakeContainer from './components/HookCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer />
+        <HookCakeContainer />
+        <CakeContainer />
+        <IceCreamContainer />
+        <NewCakeContainer />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
