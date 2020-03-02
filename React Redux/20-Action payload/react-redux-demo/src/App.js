6c3a0ea42337def8609dca605489494d87566e5b
zach chih <zach.chih@advantech.com.tw>
2020-03-02 14:41:22 +0800

    up

---
+++ React Redux/20-Action payload/react-redux-demo/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import CakeContainer from './components/CakeContainer';
+import HookCakeContainer from './components/HookCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <HookCakeContainer />
+        <CakeContainer />
+        <IceCreamContainer />
+        <NewCakeContainer />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
